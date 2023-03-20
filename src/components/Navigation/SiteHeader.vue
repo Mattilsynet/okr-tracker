96d8926f8ccb31c1d59b041aec64953821874b85
Martin Langvatn <martin.langvatn@gmail.com>
2023-03-20 16:39:51 +0100

    feat: add mattilsynet styling

--- src/components/Navigation/SiteHeader.vue
+++ src/components/Navigation/SiteHeader.vue
@@ -79,7 +79,7 @@ export default {
   height: 4rem;
   padding: 0 0.6875rem;
   color: var(--color-text-secondary);
-  background: var(--color-primary);
+  background: #D04234;
 
   &__title {
     font-weight: 500;
